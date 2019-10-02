5f5f5710aae6732c5828f9af738cf7e1ad67b9d1
Dennis Padiernos <depadiernos@gmail.com>
2019-10-02 15:30:50 -0500

    scaffold the components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, {Component} from "react"
 
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+import TodoList from "components/TodoComponents/TodoList"
+import TodoForm from "components/TodoComponents/TodoForm"
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: "Bake Cookies",
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+  onComplete = (event, id) => {
+    this.setState(
+      this.state.list.map((item) => {
+        if (item.id === id) {
+          item.completed = !item.completed
+        }
+      })
+    )
+  }
+
+  onAddItem = (newItem) => {
+    this.setState({ list: [...this.state.list, newItem] })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoForm onClick={this.onAddItem} />
+        <TodoList list={this.state.list} onClick={this.onComplete} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, {Component} from 'react';
+
+class TodoForm extends Component {
+constructor(props){
+  super(props)
+  this.state = {
+    
+  }
+}
+
+
+
+render(){
+  return <div></div>
+}
+
+}
+
+export default TodoForm
